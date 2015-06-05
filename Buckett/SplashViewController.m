d5cdeaf4db46a902a5cf7cba55b8d503ab86ca47
Harpreet Singh <harpreet@42works.net>
2015-06-05 14:33:24 +0530

    i have added slog in splash view controller

---
+++ Buckett/SplashViewController.m
@@ -0,0 +1,39 @@
+//
+//  SplashViewController.m
+//  
+//
+//  Created by Anmol Rajdev on 05/06/15.
+//
+//
+
+#import "SplashViewController.h"
+
+@interface SplashViewController ()
+
+@end
+
+@implementation SplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    NSLog(@"skjdkjsdhlkasjdlksajdklsagdhasgdsadk");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
